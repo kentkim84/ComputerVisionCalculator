9c47a67340ab837e22183ff2245c7c9f6973a83a
Yongjin <kentkim84@gmail.com>
2018-02-22 23:27:51 +0000

    script running sequence checked

--- cascade_training.sh
+++ cascade_training.sh
@@ -14,11 +14,13 @@ sudo sh -c '/bin/echo 3 > /proc/sys/vm/drop_caches'
 free
 echo 
 # -Preparations-
-PRE_DIR="${r#/home/ubuntu/opencv_workplace/}"
-NEW_DIR="${PRE_DIR%.*}"
+IMG_NAME="${r#/home/ubuntu/opencv_workplace/}"
+NEW_DIR="${IMG_NAME%.*}"
 echo "Process directory with an image file: $r"
+echo "IMG_NAME: $IMG_NAME"
+echo "NEW_DIR: $NEW_DIR"
 echo
-echo -e "\e[31;43m***** PREPARE FILE AND FOLDER *****\e[0m"
+echo -e "\e[31;43m***** CLEANUP FILE AND FOLDER *****\e[0m"
 if [ -r ~/opencv_workplace/data ]; then
 mv ~/opencv_workplace/data ~/opencv_workplace/resources/"$NEW_DIR"
 echo "Data folder moved to resource folder"
@@ -37,24 +39,28 @@ echo "Vector file moved to resource folder"
 else
 echo "Vector file doesn't exist"
 fi
-echo "Preparation Finished"
+echo "CLEANUP Finished"
 echo
 # Phase 1: Create positive images
 echo -e "\e[31;43m***** CREATE POSITIVE IMAGES *****\e[0m"
 # - Create the 'info' folder then generate positive images
 ## only test purpose ##
-mkdir ~/opencv_workplace/info
+## mkdir ~/opencv_workplace/info
+opencv_createsamples -img ~/opencv_workplace/"$IMG_NAME" -bg ~/opencv_workplace/bg.txt -info ~/opencv_workplace/info/info.lst -pngoutput info -maxxangle 0.5 -maxyangle 0.5 -maxzangle 0.5 -num 2000
 echo "Info folder created"
 # Phase 2: Create vertor a vector file
 echo -e "\e[31;43m***** CREATE A VERTOR FILE *****\e[0m"
 ## only test purpose ##
-echo "test" >> ~/opencv_workplace/test.vec
+## echo "test" >> ~/opencv_workplace/test.vec
+opencv_createsamples -info ~/opencv_workplace/info/info.lst -num 2000 -w 20 -h 20 -vec ~/opencv_workplace/positives.vec
 echo "Vector file created"
 # Phase 3: Start train
 echo -e "\e[31;43m***** TRAIN AND CREATE CASCADE CLASSIFIER *****\e[0m"
 # - Create the 'data' folder for the next positive source
 mkdir ~/opencv_workplace/data
 echo "Data folder created"
+echo "Train Start"
+opencv_traincascade -data ~/opencv_workplace/data -vec ~/opencv_workplace/positives.vec -bg ~/opencv_workplace/bg.txt -numPos 1800 -numNeg 900 -numStages 10 -w 20 -h 20
 echo "Train finished"
 # Phase 4: Move trained data
 if [ -r ~/opencv_workplace/data ]; then
